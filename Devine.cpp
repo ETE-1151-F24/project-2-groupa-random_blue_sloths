a9ca0fdd52516c06c0ddae081b9d690197e76452
DevinFoster <dlfoster@cpp.edu>
2024-10-23 14:52:26 -0700

    Project2Outline_DevinFoster

---
+++ Devine.cpp
@@ -0,0 +1,35 @@
+/*
+This file has functions for the map, 
+leveling, and save mechanics. 
+*/
+
+#include<iostream>
+#include"Devine.hpp"
+using namespace std;
+
+namespace Map{ 
+
+    void Print(){
+
+    }
+    
+
+} // Ending namespace Map
+
+
+
+namespace Level{ 
+
+    void Increase(){
+
+    }
+
+} // Ending namespace Level
+
+
+
+namespace Save{ 
+
+} // Ending namespace Save
+
+
---
+++ Devine.hpp
@@ -0,0 +1,41 @@
+/*
+This file is used for declaring the map, 
+leveling, and save mechanics. 
+*/
+
+// these is needed for header files 
+#ifndef Devine_h 
+#define Devine_h
+
+#include<iostream>
+
+
+
+namespace Map{ 
+
+    void Print();
+
+
+} // Ending namespace Map
+
+
+
+namespace Level{ 
+
+    void Increase();
+
+
+} // Ending namespace Level
+
+
+
+namespace Save{
+    
+
+} // Ending namespace Save
+
+
+
+
+
+#endif // This is needed for header files 
